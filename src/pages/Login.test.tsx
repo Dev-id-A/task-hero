6be7fa771fb9e37e695e7c603557b1f30dc76e72
Dev-id-A <davidaranzana618@gmail.com>
2025-10-13 11:09:59 +0200

    Languague button test passed

--- src/pages/Login.test.tsx
+++ src/pages/Login.test.tsx
@@ -3,14 +3,49 @@ import Login from "./Login";
 import { describe, it, expect } from "vitest";
 import "@testing-library/jest-dom/vitest"
 import { MemoryRouter } from "react-router";
+import userEvent from "@testing-library/user-event"
+import { useState, useRef } from "react";
+
+function LoginPage() {
+    const [lang, setLang] = useState<"es" | "en">("es")
+    const user = useRef<HTMLInputElement | null>(null)
+    
+
+    const toggleFade = (langParam : "es" | "en")=>{
+        setLang(langParam)
+    };
+    return(
+        <MemoryRouter>
+            <Login lang={lang} toggleFade={toggleFade} nightMode={false} user={user}/>
+        </MemoryRouter>
+    );
+}
+
 
 describe("Login", () =>{
-    it("Looking if tests run", ()=>{
-        render(
-        <MemoryRouter>
-            <Login lang="es" toggleFade={function(){}}/>
-        </MemoryRouter>
-        );
-        expect(screen.getByText("Login")).toBeInTheDocument()
+    it("Changes the language", async ()=>{
+        render(<LoginPage />);
+        const esButton = screen.getByAltText("Spanish icon")
+        const enButton = screen.getByAltText("English icon")
+
+        expect(screen.getByText("Bienvenido")).toBeInTheDocument()
+        expect(screen.getByText("Idioma")).toBeInTheDocument()
+        expect(screen.getByText("Entrar")).toBeInTheDocument()
+        expect(screen.getByPlaceholderText("Introduzca su nombre")).toBeInTheDocument()
+
+        await userEvent.click(enButton);
+
+        expect(await screen.findByText("Welcome")).toBeInTheDocument()
+        expect(await screen.findByText("Language")).toBeInTheDocument()
+        expect(await screen.findByText("Enter")).toBeInTheDocument()
+        expect(await screen.findByPlaceholderText("Enter your name")).toBeInTheDocument()
+
+        await userEvent.click(esButton);
+
+        expect(await screen.findByText("Bienvenido")).toBeInTheDocument()
+
+
+        
     });
+
 })
