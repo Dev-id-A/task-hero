480c3bddf6bc40638757e81c4c7075b2d8e664cc
Dev-id-A <davidaranzana618@gmail.com>
2025-10-15 11:53:41 +0200

    Difficult added

--- src/assets/Json/HomeJson.tsx
+++ src/assets/Json/HomeJson.tsx
@@ -15,12 +15,37 @@ export const homeJson = {
         es: "Veces hecha:",
         en: "Times done:"
     },
-    complete:{
-        es: "Completar",
-        en: "Complete"
+    difficult:{
+        es: "Dificultad:",
+        en: "Difficult:"
+    },
+    add:{
+        es: "Añadir tarea",
+        en: "Add task"
     },
     taskExample:{
         es: "Batalla épica contra los platos sucios",
         en: "Epic battle against dirty dishes"
-    }
+    },
+    //Difficulty levels
+    veryEasy:{
+        es: "Muy fácil",
+        en: "Very easy"
+    }, 
+    easy:{
+        es: "Fácil",
+        en: "Easy"
+    }, 
+    normal:{
+        es: "Normal",
+        en: "Normal"
+    }, 
+    hard:{
+        es: "Difícil",
+        en: "Hard"
+    }, 
+    veryHard:{
+        es: "Muy difícil",
+        en: "Very hard"
+    },
 } as const;
--- src/assets/TaskInput.tsx
+++ src/assets/TaskInput.tsx
@@ -4,8 +4,8 @@ import type { Lang } from "../App"
 function TaskInput({lang}:
   {lang:Lang}) {
   return (
-    <div className="size-full flex flex-col -center justify-center gap-15 px-2 text-center text-xl">
-      <h2>{homeJson.addTask[lang]}</h2>
+    <div className="size-full flex flex-col -center justify-center gap-8 px-5 text-center text-2xl">
+      <h2 className="text-3xl">{homeJson.addTask[lang]}</h2>
 
       <div className="flex flex-col gap-1">
         <h3>{homeJson.task[lang]}</h3>
@@ -14,10 +14,21 @@ function TaskInput({lang}:
       
       <div className="flex flex-row self-center gap-1">
         <h3>{homeJson.times[lang]}</h3>
-        <input className="w-8 border-1 rounded-sm" type="number" min={1} step={1} />
+        <input className="w-1/2 h-fit border-1 rounded-sm" type="number" min={1} max={1000} step={1} />
       </div>
 
-      <button className="border-1 p-1 bg-green-300 cursor-pointer mx-10">{homeJson.complete[lang]}</button>
+      <div className="flex flex-row items-center gap-1">
+        <h3>{homeJson.difficult[lang]}</h3>
+        <select className="w-full text-center" name="difficult" id="select-difficult">
+          <option value="very easy">{homeJson.veryEasy[lang]}</option>
+          <option value="easy">{homeJson.easy[lang]}</option>
+          <option value="normal">{homeJson.normal[lang]}</option>
+          <option value="hard">{homeJson.hard[lang]}</option>
+          <option value="very hard">{homeJson.veryHard[lang]}</option>
+        </select>
+      </div>
+
+      <button className="border-1 p-1 bg-green-300 cursor-pointer mx-10">{homeJson.add[lang]}</button>
     </div>
   )
 }
