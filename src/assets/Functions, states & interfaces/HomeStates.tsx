c1b21b0b42ec8f92694a2fbf08f63973d4a4125c
Dev-id-A <davidaranzana618@gmail.com>
2025-11-07 11:00:22 +0100

    States moved to HomeStates

---
+++ src/assets/Functions, states & interfaces/HomeStates.tsx
@@ -0,0 +1,66 @@
+import { useState, useRef } from "react";
+import type { newTaskInterface } from "./Types&Interfaces";
+
+    export function taskState(){
+    const [allTaskState, setAllTaskState] = useState<newTaskInterface[]>(()=>{
+    const savedTask = localStorage.getItem("savedTask");
+
+    return savedTask ? JSON.parse(savedTask):[];
+    });
+    const [dailyTaskState, setDailyTaskState] = useState<newTaskInterface[]>([]);
+    const [addTask, setAddTask] = useState<boolean>(false);
+    return{
+        allTaskState, setAllTaskState,
+        dailyTaskState, setDailyTaskState,
+        addTask, setAddTask
+    }
+}
+
+    export function levelStates(){
+        //Leveling states
+    const [percentage, setPercentage] = useState<number>(0);
+    const [eraseXPBar, setEraseXPBar] = useState<boolean>(false);
+        //Saved states
+        const [level, setLevel] = useState<number>(()=>{
+        const actualLevel = localStorage.getItem("level");
+
+        return actualLevel ? Number(actualLevel):1;
+        });
+        const [actualXP, setActualXP] = useState<number>(()=>{
+        const userActualXP = localStorage.getItem("actualXP");
+
+        return userActualXP ? Number(userActualXP):0;
+        });
+        const [maxXP, setMaxXP] = useState<number>(()=>{
+        const userActualXP = localStorage.getItem("maxXP");
+
+        return userActualXP ? Number(userActualXP):100;
+        });
+
+        return{
+            percentage, setPercentage,
+            eraseXPBar, setEraseXPBar,
+            level, setLevel,
+            actualXP, setActualXP,
+            maxXP, setMaxXP
+        }
+    }
+
+    export function windowsState(){
+        const [alertWindow, setAlertWindow] = useState<boolean>(false);   
+        const [levelUpWindow, setLevelUpWindow] = useState<boolean>(false);   
+
+        return {
+            alertWindow, setAlertWindow,
+            levelUpWindow, setLevelUpWindow
+        }
+    }
+
+    export function taskEraseState(){
+        const [eraseTaskState, setEraseTaskState] = useState<boolean>(false);
+        const taskToErase = useRef<number | null>(null);
+
+        return{
+            eraseTaskState, setEraseTaskState, taskToErase
+        }
+    }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,54 +1,32 @@
-import { useEffect, useRef, useState} from "react"
-import type { propsType, newTaskInterface } from "../assets/Functions, states & interfaces/Types&Interfaces"; 
+import { useEffect, useState} from "react"
+import type { propsType } from "../assets/Functions, states & interfaces/Types&Interfaces"; 
 import AlertWindow from "../assets/Windows/AlertWindow";
 import LevelUpWindow from "../assets/Windows/LevelUpWindow";
 import Navbar from "../assets/Home components/Navbar";
 import TaskAccordion from "../assets/Home components/TaskAccordion";
 import { homeJson } from "../assets/Json/HomeJson";
-import { animationXPBar, reduceTimes, saveLevelAndXP } from "../assets/Functions, states & interfaces/TaskFunctions";
+import { animationXPBar, reduceTimes, saveLevelAndXP } from "../assets/Functions, states & interfaces/HomeFunctions";
+import { taskState, levelStates, windowsState, taskEraseState } from "../assets/Functions, states & interfaces/HomeStates";
 
 
 function Home({lang, setLang, nightMode, setNightMode}:propsType) {
 
   //Task states
-  const [allTaskState, setAllTaskState] = useState<newTaskInterface[]>(()=>{
-    const savedTask = localStorage.getItem("savedTask");
+  const {allTaskState, setAllTaskState, dailyTaskState, setDailyTaskState, addTask, setAddTask} = taskState();
 
-    return savedTask ? JSON.parse(savedTask):[];
-  });
-  const [dailyTaskState, setDailyTaskState] = useState<newTaskInterface[]>([]);
-  const [addTask, setAddTask] = useState<boolean>(false);
+  //Erasing task
+  const {eraseTaskState, setEraseTaskState, taskToErase} = taskEraseState();
 
   //Menu state
   const [openMenu, setOpenMenu] = useState<boolean>(false);
-  
+
   //Leveling states
-  const [percentage, setPercentage] = useState<number>(0);
-  const [eraseXPBar, setEraseXPBar] = useState<boolean>(false);
-    //Saved states
-    const [level, setLevel] = useState<number>(()=>{
-      const actualLevel = localStorage.getItem("level");
-
-      return actualLevel ? Number(actualLevel):1;
-    });
-    const [actualXP, setActualXP] = useState<number>(()=>{
-      const userActualXP = localStorage.getItem("actualXP");
-
-      return userActualXP ? Number(userActualXP):0;
-    });
-    const [maxXP, setMaxXP] = useState<number>(()=>{
-      const userActualXP = localStorage.getItem("maxXP");
-
-      return userActualXP ? Number(userActualXP):100;
-    });
+  const{ percentage, setPercentage, eraseXPBar, setEraseXPBar,
+  //Saved leveling states
+    level, setLevel, actualXP, setActualXP, maxXP, setMaxXP} = levelStates();
 
   //Windows states
-  const [alertWindow, setAlertWindow] = useState<boolean>(false);   
-  const [levelUpWindow, setLevelUpWindow] = useState<boolean>(false);   
-
-  //Erasing task
-  const [eraseTaskState, setEraseTaskState] = useState<boolean>(false);
-  const taskToErase = useRef<number | null>(null);
+  const {alertWindow, setAlertWindow, levelUpWindow, setLevelUpWindow} = windowsState();
 
   //Title state
   const [showTitle, setShowTitle] = useState<boolean>(false);
@@ -79,16 +57,16 @@ function Home({lang, setLang, nightMode, setNightMode}:propsType) {
       <LevelUpWindow {...{lang, level, levelUpWindow, setLevelUpWindow}}/>
       <AlertWindow {...{lang, alertWindow, setAlertWindow, setEraseTaskState}}/>
 
-          <Navbar user={{lang, username}} title={{showTitle, setShowTitle}} 
+        <Navbar user={{lang, username}} title={{showTitle, setShowTitle}} 
           levelOptions={{level, percentage, eraseXPBar}} menu={{openMenu, setOpenMenu}} night={{nightMode, setNightMode}} />
 
         <TaskAccordion title={homeJson.normalTask[lang]} user={{lang, setAlertWindow}} taskState={allTaskState} setTaskState={setAllTaskState}
-        taskCreate={{addTask, setAddTask}}  taskErase={{eraseTask, eraseTaskState, setEraseTaskState, taskToErase, 
-        reduceTimes:(id)=>reduceTimes(id, setAllTaskState, setActualXP)}}/>
+          taskCreate={{addTask, setAddTask}}  taskErase={{eraseTask, eraseTaskState, setEraseTaskState, taskToErase, 
+          reduceTimes:(id)=>reduceTimes(id, setAllTaskState, setActualXP)}}/>
 
         <TaskAccordion title={homeJson.dailyTask[lang]} user={{lang, setAlertWindow}} taskState={dailyTaskState} setTaskState={setDailyTaskState}
-        taskCreate={{addTask, setAddTask}}  taskErase={{eraseTask, eraseTaskState, setEraseTaskState, taskToErase, 
-        reduceTimes:(id)=>reduceTimes(id, setAllTaskState, setActualXP)}}/>
+          taskCreate={{addTask, setAddTask}}  taskErase={{eraseTask, eraseTaskState, setEraseTaskState, taskToErase, 
+          reduceTimes:(id)=>reduceTimes(id, setAllTaskState, setActualXP)}}/>
 
     </main>
           
