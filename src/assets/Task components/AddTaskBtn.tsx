4229f4f1c8e064b6c08148068f99ac2eccd96851
Dev-id-A <davidaranzana618@gmail.com>
2025-11-12 18:04:43 +0100

    framer motion installed and tested

--- src/assets/Task components/AddTaskBtn.tsx
+++ src/assets/Task components/AddTaskBtn.tsx
@@ -1,9 +1,18 @@
 import type { Dispatch, SetStateAction } from "react"
+import {easeIn, motion} from "framer-motion"
 
 function AddTaskBtn({setAddTask}:{setAddTask: Dispatch<SetStateAction<boolean>>}) {
   return (
     <button onClick={()=> setAddTask(true)} className="bg-blue-200 flex align-center justify-center cursor-pointer">
-        <img className="size-24 border-3 border-black bg-blue-300" src="/svg/plus.svg" alt="Plus image" />
+        <motion.img 
+        initial={{rotate: 0}}
+        animate={{rotate: 180}}
+        transition={{
+          duration: 2,
+          ease: easeIn
+        }}
+        exit={{rotate: 0}}
+        className="size-24 border-3 border-black bg-blue-300" src="/svg/plus.svg" alt="Plus image" />
     </button>
   )
 }
