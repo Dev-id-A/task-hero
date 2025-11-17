91038bf5e21891880386f0ac0c7d705d9e652b55
Dev-id-A <davidaranzana618@gmail.com>
2025-11-17 18:14:54 +0100

    Scale animation on the tasks div

--- src/assets/Home components/TaskAccordion.tsx
+++ src/assets/Home components/TaskAccordion.tsx
@@ -4,6 +4,8 @@ import TaskToDo from "../Task components/TaskToDo"
 import TaskInput from "../Task components/TaskInput"
 import AddTaskBtn from "../Task components/AddTaskBtn"
 import type { Dispatch, RefObject, SetStateAction } from "react"
+import {easeIn, motion} from "framer-motion"
+
 
 function TaskAccordion({title, user, taskCreate, taskErase, taskState, setTaskState, recurrent, alertWindowsStates}:
   {title:string, taskState: newTaskInterface[], setTaskState: Dispatch<SetStateAction<newTaskInterface[]>>, recurrent?: RefObject<boolean>,
@@ -20,21 +22,39 @@ function TaskAccordion({title, user, taskCreate, taskErase, taskState, setTaskSt
             [&>svg]:size-8 [&>svg]:duration-500 [&>svg]:text-black">
               {title}</AccordionTrigger>
             <AccordionContent>
-              <section className="md:grid md:grid-cols-2 lg:grid-cols-3">
+              <section 
+              
+              className="md:grid md:grid-cols-2 lg:grid-cols-3">
                 {taskState.map((object, i)=>{
-                  return <div key={i + "div"} className={`bg-blue-200 min-h-100 flex flex-col items-center justify-center border-3 border-black m-8
+                  return <motion.div 
+                  initial={{scale: 0}}
+                  animate={{scale: 1}}
+                  style={{originX: 0.5, originY: 0.5}}
+                  transition={{
+                    duration: 0.5,
+                    ease: easeIn
+                  }}
+                  key={i + "div"} className={`bg-blue-200 min-h-100 flex flex-col items-center justify-center border-3 border-black m-8
                   ${object.completed && "hidden"}`}>
                       <TaskToDo key={i} 
                       {...{lang, object, reduceTimes, eraseTask, setEraseWindow,
                       eraseTaskState, setEraseTaskState, taskToErase}} />
-                    </div>
+                    </motion.div>
                 })}
       
-                <div className="bg-blue-200 min-h-100 flex flex-col items-center justify-center border-3 border-black m-8">
+                <motion.div
+                  initial={{scale: 0}}
+                  animate={{scale: 1}}
+                  style={{originX: 0.5, originY: 0.5}}
+                  transition={{
+                    duration: 0.5,
+                    ease: easeIn
+                  }}
+                className="bg-blue-200 min-h-100 flex flex-col items-center justify-center border-3 border-black m-8">
                   {addTask ?
                   (<TaskInput {...{lang, taskState, setTaskState, setAddTask, recurrent, alertWindowsStates}}/>):
                   (<AddTaskBtn {...{addTask, setAddTask}}/>)}
-                </div>
+                </motion.div>
               </section>
             </AccordionContent>
         </AccordionItem>
--- src/assets/Task components/AddTaskBtn.tsx
+++ src/assets/Task components/AddTaskBtn.tsx
@@ -1,17 +1,10 @@
 import type { Dispatch, SetStateAction } from "react"
-import {easeIn, motion} from "framer-motion"
 
 function AddTaskBtn({setAddTask}:{setAddTask: Dispatch<SetStateAction<boolean>>}) {
   return (
     <button onClick={()=> setAddTask(true)} className="bg-blue-200 flex align-center justify-center cursor-pointer">
-        <motion.img 
-        initial={{rotate: 0}}
-        animate={{rotate: 180}}
-        transition={{
-          duration: 2,
-          ease: easeIn
-        }}
-        exit={{rotate: 0}}
+        <img 
+        
         className="size-24 border-3 border-black bg-blue-300" src="/svg/plus.svg" alt="Plus image" />
     </button>
   )
