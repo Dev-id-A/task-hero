8e6946285c20acab81948ec875099cc67a135dc9
Dev-id-A <davidaranzana618@gmail.com>
2025-11-19 16:58:02 +0100

    Test excluded for deploy

--- src/assets/Json/AlertsJson.tsx
+++ src/assets/Json/AlertsJson.tsx
@@ -346,5 +346,46 @@ export const titlesJson = [
 {
   es: "Creador del recado original",
   en: "Creator of the original errand"
-}
+},
+  //Ninth ten levels 
+// {
+//   es: "Recadero espacio-temporal",
+//   en: "Space-temporal tasker"
+// },
+// {
+//   es: "Profeta de los recados",
+//   en: "Prophet of errands"
+// },
+// {
+//   es: "Apóstol de los recados",
+//   en: "Apostle of errands"
+// },
+// {
+//   es: "Padre del recado",
+//   en: "Father of the errand"
+// },
+// {
+//   es: "Recadero bendecido",
+//   en: "Blessed tasker"
+// },
+// {
+//   es: "Divinidad de los recados",
+//   en: "Divinity of errands"
+// },
+// {
+//   es: "El recado original",
+//   en: "The original errand"
+// },
+// {
+//   es: "Semidiós de los recados",
+//   en: "Demigod of errands"
+// },
+// {
+//   es: "Dios de los recados",
+//   en: "God of errands"
+// },
+// {
+//   es: "Creador del recado original",
+//   en: "Creator of the original errand"
+// }
 ]
